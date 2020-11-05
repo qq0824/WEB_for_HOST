3207e6f68e406b47ee65c72f461904f71c93a31b
qq0824 <59447941+qq0824@users.noreply.github.com>
2020-11-05 14:18:50 +0900

    Update app_ball.js

--- app_ball.js
+++ app_ball.js
@@ -1,3 +1,4 @@
+//탱탱볼
 import {
     Ball
 } from './ball.js';
@@ -37,7 +38,7 @@ class App {
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
 
         this.block.draw(this.ctx);
-        this.ball.draw(this.ctx, this.stageWidth, this.stageHeight);
+        this.ball.draw(this.ctx, this.stageWidth, this.stageHeight, this.block);
         
     
     }
